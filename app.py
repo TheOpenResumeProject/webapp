e94e34819f21be0b9828a41c835fe519921e53c4
MujtabaMuhammad <34104990+MujtabaMuhammad@users.noreply.github.com>
2024-09-10 18:00:48 -0400

    resolved the merge conflict in app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, jsonify
+from collections import Counter
+import re
 
 app = Flask(__name__)
 
@@ -13,9 +15,32 @@ def editor():
     return render_template('editor.html')
 
 
-@app.route('/script')
-def script():
-    return render_template('script.js')
+
+@app.route('/submit', methods=['Post'])
+def submit_data():
+    data = request.json
+    words = re.findall(r'\b\w+\b', data.lower())
+    word_counts = Counter(words)
+    # print(f'Word count: {word_counts}')
+    email = extracted_email(data)
+    phoneNumber = extracted_phoneNumber(data)
+    print(f'Data: {data}, Email: {email}, Phone: {phoneNumber}')
+    return jsonify({"received_data": data, "email": email, "phone": phoneNumber})
+
+
+def extracted_email(data):
+    email = re.findall("[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+@[a-z]+.{4}", data.lower())
+    if email:
+        return email[0]
+    return None
+
+
+def extracted_phoneNumber(data):
+    phone = re.findall(r'\d{3}\W?\d{3}\W?\d{4}', data.lower())
+    if phone:
+        return phone[0]
+    return None
+
 
 
 if __name__ == '__main__':
