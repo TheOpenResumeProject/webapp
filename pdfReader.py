3b8a14f6541b350d4e88f54232f936a29dfeb4bd
MujtabaMuhammad <34104990+MujtabaMuhammad@users.noreply.github.com>
2024-10-06 13:32:21 -0400

    This inlcudes function for get educational experince out of the resume

--- pdfReader.py
+++ pdfReader.py
@@ -1,8 +1,12 @@
 from pypdf import PdfReader
 import re
 
+def cleaner(list_array, item):
+    res = [i for i in list_array if i != item]
+    return res
 
-def pefReader(data):
+def pdfReader(data):
+    item =''
     reader = PdfReader(data)
     content = reader.pages[0].extract_text()
     content_lower = content.lower()
@@ -12,12 +16,14 @@ def pefReader(data):
     pattern_2 = r' {2,}'
     results = re.sub(pattern_2, '  ', elements)
     result_arr = results.split(' ')
-    for i, x in enumerate(result_arr):
-        if x == 'education':
-            for j in range(i - 2, i + 120):
-                education_array.append(content_array[j])
-    education_exp = ' '.join(education_array)
-    #print(content_array)
-    print(education_exp)
+    result_arr.remove('')
+    new_arr = cleaner(result_arr, '')
+    print(new_arr[0])
+    y = new_arr.index('\nwork')
+    for i, x in enumerate(new_arr):
+        if x.strip() == "education:": #stripe function takes care of the \n that is present with education just like it is with every new line. also regex may be better # since characters like : will cause a probelm with indiviusal word search.
+           for i in range(i, y):
+                education_array.append(new_arr[i])
 
-pefReader('MMujtaba-CV.pdf')
+    print(education_array)
+pdfReader('MMujtaba-CV.pdf')
